74f8856870e0a606646b5befdb2165a1014be1c4
Samson-clement <160029018+Samson-clement@users.noreply.github.com>
2024-05-19 14:55:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+
+// Initially hide all but the first 10 images
 window.addEventListener('load', () => {
     const galleryItems = document.querySelectorAll('.gallery .item');
     galleryItems.forEach((item, index) => {
-        if (index >= 10) {
+        if (index >= 6) {
             item.classList.add('hidden');
             item.style.display = 'none';
         }
     });
 });
 
+// Function to show more images
 function showMore() {
     const hiddenItems = document.querySelectorAll('.gallery .item.hidden');
-    const itemsToShow = Array.from(hiddenItems).slice(0, 10);
+    const itemsToShow = Array.from(hiddenItems).slice(0, 6);
     itemsToShow.forEach(item => {
         item.classList.remove('hidden');
         item.style.display = 'block';
@@ -21,6 +24,7 @@ function showMore() {
     gallery.style.paddingBottom = `${currentPadding + 0}px`; // Update padding as needed
 }
 
+// Load more images when the user scrolls down
 window.addEventListener('scroll', function() {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
@@ -34,6 +38,8 @@ window.addEventListener('scroll', function() {
         }
     }
 });
+
+
 
 document.querySelectorAll('.gallery .item').forEach(item => {
     item.addEventListener('click', function() {
@@ -60,4 +66,28 @@ document.querySelectorAll('.download-img').forEach(button => {
         link.click();
         document.body.removeChild(link);
     });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the title element by its ID
+    const titleElement = document.getElementById('title');
+    
+    // Add a click event listener to the title element
+    titleElement.addEventListener('click', () => {
+        // Navigate to index.html
+        window.location.href = 'index.html';
+    });
+});
+
+
+document.getElementById('getRandomCat').addEventListener('click', function(e) {
+    e.preventDefault();
+    const randomIndex = Math.floor(Math.random() * 16) + 1;
+    const imgSrc = `./assets/${randomIndex}.png`;
+    const link = document.createElement('a');
+    link.href = imgSrc;
+    link.download = imgSrc.split('/').pop();
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 });
